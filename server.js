f9413502d8dae0de0e1a643ec647294f61f78dd7
LZylstra <lindseyzylstra@gmail.com>
2019-12-19 20:47:08 -0800

    prepared for deployment

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const MOVIEDEX = require('./movie-data.json')
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -21,12 +22,23 @@ app.use(function validateBearerToken(req, res, next) {
     // move to the next middleware
     next()
   })
+  
+// 4 parameters in middleware, express knows to treat this as error handler
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
 
   app.get('/movie', function handleGetMovie(req, res) {
     let response = MOVIEDEX;
   
     // filter our movie by genre if query param is present
-    if (req.query.genre) {
+    if (!!req.query.genre) {
       response = response.filter(movie =>
         // case insensitive searching
         movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
@@ -34,14 +46,14 @@ app.use(function validateBearerToken(req, res, next) {
     }
   
     // filter our movie by country if query param is present
-    if (req.query.country) {
+    if (!!req.query.country) {
       response = response.filter(movie =>
         movie.country.toLowerCase().includes(req.query.country.toLowerCase())
       )
     }
 
     // filter our movie by avg_vote if query param is present
-    if (req.query.avg_vote) {
+    if (!!req.query.avg_vote) {
       response = response.filter(movie => 
        Number(movie.avg_vote) >= Number(req.query.avg_vote)
       )
@@ -50,8 +62,8 @@ app.use(function validateBearerToken(req, res, next) {
     res.json(response)
   })
 
-const PORT = 8000
+  const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
+  console.log(`Server listening`)
 })
